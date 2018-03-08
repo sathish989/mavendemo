a5db595ade152db80eb209165d6a5f344f025f3d
satish <sathish9a@gmail.com>
2018-03-08 18:50:50 +0530

    maven commit

---
+++ src/main/java/marsdemo/App.java
@@ -0,0 +1,7 @@
+package marsdemo;
+
+public class App {
+public String sample(){
+	return "Sample Program";
+}
+}
---
+++ src/test/java/marsdemo/AppTest.java
@@ -0,0 +1,14 @@
+package marsdemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
